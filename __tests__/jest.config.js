576bd279282f1c4494fea5cd1f1da675e6e242c8
Caio Fuccio <caio.fuccio@gmail.com>
2023-07-21 01:13:15 -0300

    fix: modifying e2e test folder name

--- __tests__/jest.config.js
+++ __tests__/jest.config.js
@@ -5,9 +5,12 @@ const { resolve } = require('path');
 const root = resolve(__dirname, '..');
 const rootConfig = require(`${root}/jest.config.js`);
 
-module.exports = {...rootConfig, ...{
-  rootDir: root,
-  displayName: "end2end-tests",
-  setupFilesAfterEnv: ["<rootDir>/__tests__/jest-setup.ts"],
-  testMatch: ["<rootDir>/__tests__/**/*.test.ts"],
-}}
+module.exports = {
+    ...rootConfig,
+    ...{
+        rootDir: root,
+        displayName: 'end2end-tests',
+        setupFilesAfterEnv: ['<rootDir>/tests/jest-setup.ts'],
+        testMatch: ['<rootDir>/tests/**/*.test.ts'],
+    },
+};
--- jest.config.js
+++ jest.config.js
@@ -4,14 +4,14 @@
 const { resolve } = require('path');
 const root = resolve(__dirname);
 module.exports = {
-  rootDir: root,
-  displayName: 'root-tests',
-  testMatch: ['<rootDir>/src/**/*.test.ts'],
-  testEnvironment: 'node',
-  clearMocks: true,
-  preset: 'ts-jest',
-  moduleNameMapper: {
-    '@src/(.*)': '<rootDir>/src/$1',
-    '@tests/(.*)': '<rootDir>/__tests__/$1',
-  },
+    rootDir: root,
+    displayName: 'root-tests',
+    testMatch: ['<rootDir>/src/**/*.test.ts'],
+    testEnvironment: 'node',
+    clearMocks: true,
+    preset: 'ts-jest',
+    moduleNameMapper: {
+        '@src/(.*)': '<rootDir>/src/$1',
+        '@tests/(.*)': '<rootDir>/tests/$1',
+    },
 };
