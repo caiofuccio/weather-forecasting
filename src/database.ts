37a1e15f199480f8e72656d042aaf9743c7e6b78
Caio Fuccio <caio.fuccio@gmail.com>
2023-10-22 20:03:16 -0300

    feat: add github workflow config

--- src/database.ts
+++ src/database.ts
@@ -4,7 +4,15 @@ import { connect as mongooseConnect, connection } from 'mongoose';
 const dbConfig: IConfig = config.get('App.database');
 
 export const connect = async (): Promise<void> => {
-    await mongooseConnect(dbConfig.get('mongoUrl'));
+    try {
+        await mongooseConnect(dbConfig.get('mongoUrl'));
+        console.log('Connected to database successfully');
+    } catch (error) {
+        console.log(
+            'Error while trying to connect to database:',
+            (error as Error).message
+        );
+    }
 };
 
 export const close = (): Promise<void> => connection.close();
