c5f89c741f67f859710ad39610075423f8399b0b
Caio Fuccio <caio.fuccio@gmail.com>
2023-10-30 22:37:19 -0300

    feat: adding graceful shutdown and error logging

--- src/database.ts
+++ src/database.ts
@@ -1,20 +1,10 @@
 import config, { IConfig } from 'config';
-import { connect as mongooseConnect, connection } from 'mongoose';
-import logger from './logger';
+import { connection, connect as mongooseConnect } from 'mongoose';
 
 const dbConfig: IConfig = config.get('App.database');
 
 export const connect = async (): Promise<void> => {
-    try {
-        await mongooseConnect(dbConfig.get('mongoUrl'));
-        logger.info('Connected to database successfully');
-    } catch (error) {
-        logger.error(
-            `Error while trying to connect to database: ${
-                (error as Error).message
-            }`
-        );
-    }
+    await mongooseConnect(dbConfig.get('mongoUrl'));
 };
 
 export const close = (): Promise<void> => connection.close();
